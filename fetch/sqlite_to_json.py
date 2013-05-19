d2d8449f68bdb004253b0b7a8f019ca884c7549b
Lane Aasen <laneaasen@gmail.com>
2013-05-19 14:07:37 -0700

    fully working sqlite to json, now to refactor

--- fetch/sqlite_to_json.py
+++ fetch/sqlite_to_json.py
@@ -1,26 +1,39 @@
 
 import sqlite3
+import json
 
 db = 'history.sqlite'
+out = 'history.json'
 
 def get_table(cursor, table):	
 	c.execute('SELECT * FROM %s' % table)
 	return c.fetchall()
+
+def combine(a, b):
+	a = map(lambda x: list(x), a)
+	b = map(lambda x: list(x), b)
+
+	for i in xrange(len(a)):
+		for el in b[i]:
+			a[i].append(el)
+
+	return a
+
+def _add_keys(keys, values):
+	return map(lambda x: dict(zip(keys, x[:len(keys)])), values)
 
 conn = sqlite3.connect(db)
 c = conn.cursor()
 
 times = get_table(c, 'info')
 info = get_table(c, 'pages_content')
-info = map(lambda x: list(x), info)
-
-for i in xrange(len(info)):
-	info[i].append(times[i][0])
-
-keys = ['id', 'url', 'title', 'body', 'time']
-
-end = map(lambda x: dict(zip(keys, x[:len(keys)])), info)
-
-print end
 
 conn.close()
+
+data = combine(times, info)
+keys = ['time', 'id', 'url', 'title', 'body']
+
+end = _add_keys(keys, data)
+
+with open(out, 'w') as f:
+	f.write(json.dumps(end))
