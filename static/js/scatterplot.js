8f0f88a6bfc79d1fce3e445ea9dbea53fa194ac9
Lane Aasen <laneaasen@gmail.com>
2013-05-22 11:26:14 -0700

    adds plot of most visited sites

--- static/js/scatterplot.js
+++ static/js/scatterplot.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function () {
+    function getDomain(url) {
+        var a = url.split("/");
+        return a[2];
+    }
+
     var w = 800;
-    var h = 600;
-    var dataset = $.getJSON("/data/history.json", function(data) {
+    var h = 800;
+
+    function scatterplot(data) {
+        h = 50;
+
         var times = data.map(function(x) {
             return x["time"]
-        })
+        });
 
         var maxTime = d3.max(times)
         var minTime = d3.min(times)
@@ -29,5 +37,89 @@ $(document).ready(function () {
             .attr("r", function(d) {
                 return 1;
             });
+    }
+
+    function siteFrequency(data) {
+        h = 400;
+        var padding = 50;
+
+        var domains = data.map(function(x) {
+            return getDomain(x["url"]);
+        });
+
+        var counts = {};
+
+        domains.forEach(function(d, i) {
+            if (d in counts) {
+                counts[d] += 1;
+            } else {
+                counts[d] = 1;                
+            }
+        });
+
+        keys = Object.keys(counts);
+
+        var counts = keys.map(function(x) {
+            var m = {};
+            m["domain"] = x;
+            m["visits"] = counts[x];
+            return m;
+        });
+
+        var visits = counts.map(function(x) {
+            return x["visits"]
+        });
+
+        var maxVisits = d3.max(visits);
+
+        var barPadding = 4;
+
+        var data = counts;
+        var l = data.length;
+
+        console.log(counts);
+
+        var svg = d3.select("div#content").append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        var bars = svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect");
+
+        bars.attr("x", function(d, i) {
+                return i * (w / l);
+            })
+            .attr("y", function(d) {
+                return (h - ((d["visits"] / maxVisits) * h)) + padding;
+            })
+            .attr("width", function(d) {
+                return Math.max(1, w / l - barPadding);
+            })
+            .attr("height", function(d) {
+                return ((d["visits"] / maxVisits) * h);
+            });
+
+        svg.selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .text(function(d) {
+                return d["domain"];
+            })
+            .attr("x", function(d, i) {
+                return i * (w / data.length) + (w / data.length - barPadding) / 2;
+            })
+            .attr("y", function(d) {
+                console.log(d);
+                return (h + padding) - ((d["visits"] / maxVisits) * h);
+            })
+            .attr("text-anchor", "right");
+    }
+
+    var data = $.getJSON("/data/history.json", function(data) {
+        scatterplot(data);
+        siteFrequency(data);   
     });
 });
